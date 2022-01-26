aaac19cf2288bf5ead4012a92fc0d1bc5d63f51a
Pádraig <118359221@umail.ucc.ie>
2022-01-26 18:11:21 +0000

    simple compare added

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ import pandas as pd
 import vehicleProfile
 
 def main():
+    pass
+
+def testStore():
     print("Main starting!")
     df = pd.read_csv("torqueLogs\\trackLog-2022-Jan-23_19-26-14.csv")
-    profile1 = vehicleProfile.VehicleProfile('08KY12345', '16-01-22', df)
+    profile1 = vehicleProfile.VehicleProfile('08KY123Other', '16-01-26', df)
     print(profile1)
+    profile1.storeProfile()
+
+def testLoad():
+    loadedProfile = vehicleProfile.loadProfile('pickledProfiles\\08KY12345_16-01-26.pkl')
+    print(loadedProfile)
+
+def testCompare():
+    loadedProfile1 = vehicleProfile.loadProfile('pickledProfiles\\08KY12345_16-01-26.pkl')
+    loadedProfile2 = vehicleProfile.loadProfile('pickledProfiles\\08KY123Other_16-01-26.pkl')
+    loadedProfile1.compareProfiles(loadedProfile2)
+
 
 if __name__ == "__main__":
-    main()
+    #main()
+    #testStore()
+    testCompare()
--- vehicleProfile.py
+++ vehicleProfile.py
@@ -1,3 +1,5 @@
+from msilib.schema import IniFile
+import pickle
 class VehicleProfile():
     def __init__(self, reg, date, dataFrame):
         self.profileName = reg+'_'+date
@@ -8,3 +10,22 @@ class VehicleProfile():
     def __str__(self):
         return self.profileName + '\n' + str(self.profileDetails)
 
+    def storeProfile(self):
+        storeDir = 'pickledProfiles'
+        outFile = open(storeDir + '\\'+self.profileName+'.pkl', 'wb')
+        pickle.dump(self, outFile)
+        outFile.close()
+
+    def compareProfiles(self, otherProfile):
+        # naive impl currently
+        for pid in self.profileDetails.keys():
+            if self.profileDetails[pid] == otherProfile.profileDetails[pid]:
+                print(pid+' matches')
+            else:
+                print(pid+' differs!')
+
+def loadProfile(pklFile):
+    inFile = open(pklFile, 'rb')
+    loadedProfile = pickle.load(inFile)
+    inFile.close()
+    return loadedProfile
