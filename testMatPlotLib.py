e29ac22cdcb2d59e6b5e4edd5b5460451adec1a8
Pádraig <118359221@umail.ucc.ie>
2022-02-19 16:56:44 +0000

    tested out matplotlib, able to plot graphs of data

---
+++ testMatPlotLib.py
@@ -0,0 +1,29 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+
+ #Plotting to our canvas
+  
+jan16LogsPath = 'torqueLogs\\trackLog-2022-Jan-16_17-42-53.csv'
+jan23LogsPath = 'torqueLogs\\trackLog-2022-Jan-23_19-26-14.csv'
+jan27LogsPath = 'torqueLogs\\trackLog-2022-Jan-27_12-40-54.csv'
+jan29LogsPath = 'torqueLogs\\trackLog-2022-Jan-29_00-56-43.csv'
+
+df1 = pd.read_csv(jan16LogsPath)
+df2 = pd.read_csv(jan23LogsPath)
+df3 = pd.read_csv(jan27LogsPath)
+
+#plt.plot([1,2,3],[4,5,1])
+
+
+ #Showing what we plotted
+
+dfPlot = df3[['Device Time', '0100', '011c obd standards vehicle conforms to', '0120', '0903', 'CO₂ in g/km (Average)(g/km)', 'Distance travelled with MIL/CEL lit(miles)', 'EGR Commanded(%)', \
+    'Engine Coolant Temperature(°C)', 'Engine Load(%)', # 'Engine RPM(rpm)',\
+    #'Fuel Rail Pressure(psi)',\ # take out fuel rail pressure
+         'Intake Air Temperature(°C)', 'Intake Manifold Pressure(psi)', 'Mass Air Flow Rate(g/s)',\
+        'pids supported 01-20', 'service 09 pids supported', 'Turbo Boost & Vacuum Gauge(psi)', 'Voltage (OBD Adapter)(V)', \
+            'Volumetric Efficiency (Calculated)(%)']]
+
+dfPlot.plot()
+# plt.plot(df2)
+plt.show()
